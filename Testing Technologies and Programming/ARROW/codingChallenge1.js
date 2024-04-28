5072e1e45493e658a8260a35d163ed25ae27c06f
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-04-28 23:58:01 +0300

    added DB technologies folder

---
+++ Testing Technologies and Programming/ARROW/codingChallenge1.js
@@ -0,0 +1,45 @@
+
+let reverseString=name=>{
+    let reversName="";
+    for (i=0;i<name.length;i++){
+        index=name.length-1-i;
+        reversName +=name[index];
+    } 
+    return (reversName);
+}
+
+console.log(reverseString("John"));
+
+let reverseArray=array=>{
+    let reverseArray=[];
+    for (i=array.length-1;i>=0;i--){
+        reverseArray.push(array[i]);
+    }
+    return(reverseArray);
+}
+
+console.log(reverseArray(["I", "like", "JavaScript"]));
+console.log(reverseArray([5,4,3,2,1]));
+
+
+let mostExpensiveItem=items=>{
+    let cost=[];
+    max=Math.round(items[0].price*items[0].stock,1);
+    max_index=0;
+    //calculate total cost for each item and log this value in a new array
+    for (i=0;i<items.length;i++){
+        total=Math.round(items[i].price*items[i].stock,1);
+        //find the max value
+            if (max<total){
+                max=total;
+                max_index=i;
+            }
+    }
+    return(items[max_index]);
+}
+console.log(mostExpensiveItem([
+    { item: "irn bru", price: 3.25, stock: 50 },
+    { item: "fanta", price: 3.98, stock: 45 },
+    { item: "diet coke", price: 4.40, stock: 38 }, 
+    { item: "7up", price: 3.99, stock: 42 }, 
+ ]));
--- Testing Technologies and Programming/ARROW/main.js
+++ Testing Technologies and Programming/ARROW/main.js
@@ -1,3 +1,31 @@
 
-let show = name=> "hello, my name is" +name;
+let show = name=> "Hi " +name +"!";
 console.log(show("John"));
+console.log(show("Olha"));
+
+
+let isEven=num=>num % 2 === 0;
+console.log(isEven(7));
+console.log(isEven(8));
+
+
+let counterFunc=counter=>{
+if (counter > 100) {
+    counter = 0;
+    } else {
+    counter++;
+    }
+    
+    return counter;
+}
+console.log(counterFunc(10));
+
+let nameAge=(name,age)=>{
+    console.log("Hello " + name);
+    console.log("You are " + age + " years old");
+}
+
+let printOnly=()=>console.log("printing");
+
+let sum=(num1,num2)=>num1+num2;
+console.log(sum(3,4));
